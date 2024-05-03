8bfbf9384b4ce1b20f91166b9ddc938f1c3cb6e3
Rufaidah Jomaa <rufaidah.ahmad.jomaa@gmail.com>
2024-05-03 20:22:43 +0300

    complete category

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -16,3 +16,51 @@ export const createCategory = async(req,res)=>{
     const category = await categoryModel.create({name,image:{secure_url,public_id},slug:slugify(name)})
     return res.status(200).json({message:"success",category})
 }
+
+export const getAll = async(req,res)=>{
+    const categories = await categoryModel.find({}).select('name')
+    return res.status(200).json({message:"success",categories})
+}
+
+export const getActive= async (req,res)=>{
+    const categories = await categoryModel.find({status:'Active'})
+    return res.status(200).json({message:"success",categories})
+}
+
+export const getDetails = async (req,res)=>{
+    const category = await categoryModel.findById(req.params.id)
+    if(!category){
+        return res.json({message:"category not found"})
+    }
+    return res.status(200).json({message:"success",category})
+}
+
+export const update = async(req,res)=>{
+   const category = await categoryModel.findById(req.params.id)
+   if(! category){
+    return res.status(404).json("category not found")
+   }
+  category.name=req.body.name.toLowerCase();
+   if (await categoryModel.findOne({name:category.name , _id:{$ne:req.params.id}})){
+    return res.status(409).json("This name already exists")
+   }
+category.slug =slugify(req.body.name.toLowerCase())
+category.status = req.body.status
+if(req.file){
+    const {secure_url,public_id}= await cloudinary.uploader.upload(req.file.path,
+        {folder:`${process.env.App_Name}/catigories`})
+    await cloudinary.uploader.destroy(category.image.public_id)
+    category.image = {secure_url,public_id}
+}
+  category.save()
+   return res.json({message:"success",category})
+}
+
+export const destroy = async(req,res)=>{
+    const category = await categoryModel.findByIdAndDelete(req.params.id)
+    if(!category){
+        return res.status(404).json({message:"Category Not Found!"})
+    }
+    await cloudinary.uploader.destroy(category.image.public_id)
+    return res.json({message:"category successfully deleted",category})
+}
--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -6,5 +6,9 @@ const router = Router();
 
 router.get('/',controller.testCategory)
 router.post('/create',uploadFile(fileType.image).single('image'),asyncHandler(controller.createCategory))
-
+router.get('/getAll',asyncHandler(controller.getAll))
+router.get('/getActive',asyncHandler(controller.getActive))
+router.get('/getDetails/:id',asyncHandler(controller.getDetails))
+router.patch('/update/:id',uploadFile(fileType.image).single('image'),asyncHandler(controller.update))
+router.delete('/delete/:id',asyncHandler(controller.destroy))
 export default router;
