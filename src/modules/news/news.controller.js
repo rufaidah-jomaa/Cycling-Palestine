62c8f7aaf1976b496675fac5c371b30e63bebc8e
Rufaidah Jomaa <rufaidah.ahmad.jomaa@gmail.com>
2024-06-13 20:41:05 +0300

    remove return statement

--- src/modules/news/news.controller.js
+++ src/modules/news/news.controller.js
@@ -6,7 +6,6 @@ export const add = async(req,res,next)=>{
 
     const {title,content,date}=req.body;
     if(req.files.video){
-      return res.json(req.files.video)
     const {secure_url,public_id}= await cloudinary.uploader.upload(req.files.video[0].path,
        { resource_type: 'video',
         folder:`${process.env.App_Name}/news/${title}`})
@@ -47,7 +46,7 @@ export const update = async(req,res,next)=>{
    if(req.files.video){
         const {secure_url,public_id}= await cloudinary.uploader.upload(req.files.video[0].path,
             {resource_type: 'video',
-          folder:`${process.env.App_Name}/news/${req.body.title}`})
+          folder:`${process.env.App_Name}/news/${theNew.title}`})
         await cloudinary.uploader.destroy(theNew.video.public_id)
         theNew.video = {secure_url,public_id}
     }
@@ -63,7 +62,7 @@ export const update = async(req,res,next)=>{
          theNew.images=[]
       for (const file of req.files.images) { // file: iteration   
         const {secure_url,public_id} = await cloudinary.uploader.upload(file.path,
-               {folder:`${process.env.App_Name}/news/${req.body.title}`})
+               {folder:`${process.env.App_Name}/news/${theNew.title}`})
                req.body.images.push({secure_url,public_id})
        }
        theNew.images=req.body.images
