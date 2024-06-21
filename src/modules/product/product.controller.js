f84b2ec3fbe8df29de36eb507e50a9d8b4c5ae97
Rufaidah Jomaa <rufaidah.ahmad.jomaa@gmail.com>
2024-06-21 13:45:22 +0300

    search in all

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -68,8 +68,17 @@ export const addProductsExcel=async(req,res,next)=>{
 
 export const getAll = async(req,res,next)=>{
    // const {skip,limit}= pagination(req.query.page,req.query.limit)
-
-    const products = await productModel.find({status:"Active"}).select('name price mainImage')//.skip(skip).limit(limit)
+   // const products = await productModel.find({status:"Active"}).select('name price mainImage')//.skip(skip).limit(limit)
+    const mongooseQuery =  productModel.find({status:"Active"}).select('name price mainImage')//.skip(skip).limit(limit)
+    if(req.query.search){
+    mongooseQuery.find({
+        $or:[
+       { name:{$regex:req.query.search}},
+       {description:{$regex:req.query.search}}
+        ]
+    })
+}
+    const products= await mongooseQuery.sort(req.query.sort);
     return res.status(200).json({message:"success",products})
 }
 
